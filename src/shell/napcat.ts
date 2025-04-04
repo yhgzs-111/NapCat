62b04007bb3dccd1fa1cc0140edcbc89f4750cbd
手瓜一十雪 <nanaeonn@outlook.com>
2025-04-04 13:29:00 +0800

    fix

--- src/shell/napcat.ts
+++ src/shell/napcat.ts
@@ -1,4 +1,4 @@
-import { convertAudio } from '@/common/audio_pure';
+import { convertAudio } from '@/common/audio-pure';
 import { NCoreInitShell } from './base';
 await convertAudio("F:\\BVideo\\陈致逸,HOYO-MiX - 蒲苇如丝 Lovers' Oath.mp3", "F:\\BVideo\\陈致逸,HOYO-MiX - 蒲苇如丝 Lovers' Oath.wav", "wav");
 NCoreInitShell();
